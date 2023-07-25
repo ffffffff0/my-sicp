6301f082f74ebeb0649d4537500515a44d9f5f5a
wen xu <xuwenmail@foxmail.com>
2023-07-25 22:02:47 +0800

    update 1.31.rkt

--- char1/scheme/1.31.rkt
+++ char1/scheme/1.31.rkt
@@ -1,51 +1,44 @@
 #lang sicp
 
-(define (isEven n)
-  (= remainder(n 2) 0)
-)
-; product function  recu
+; product function recu
 (define (product term a next b)
   (if (> a b)
       1
-      (* (term a) (product term (next a) next b))
-  )
-)
+      (* (term a) (product term (next a) next b))))
 
 ; product function item
-(define (product term a next b)
-  (define (iter a result)
-    (if (> a b)
-      result
-      (item (+ a 1) (* (term a) result))))
-  (iter a 1)
-)
+; (define (product term a next b)
+;   (define (iter-item a result)
+;     (if (> a b) result (iter-item (next a) (* (term a) result))))
+;   (iter-item a 1))
 
 ; gen numberator
 (define (number-item i)
-  (cond ((= i 1) 2)
-        ((isEven i) (+ i 2))
-        (else (+ i 1))
-  )
-)
+  (cond
+    [(= i 1) 2]
+    [(even? i) (+ i 2)]
+    [else (+ i 1)]))
 
 ; gen denominator
 (define (deno-item i)
-  (if (isEven i)
-      (+ i 1)
+  (if (odd? i)
       (+ i 2)
-  )
-)
+      (+ i 1)))
 
 ; recu
 (define (pi n)
-  (* 4 (exact->inexact(/ (product number-item 1 (lambda (i) (+ i 1)) n)
-          (product deno-item 1 (lambda (i) (+ i 1)) n))
-       )
-  )
-)
+  (* 4
+     (exact->inexact
+      (/ (product number-item
+                  1
+                  (lambda (i) (+ i 1))
+                  n)
+         (product deno-item
+                  1
+                  (lambda (i) (+ i 1))
+                  n)))))
 
 ; iter
-
 (pi 10)
 (pi 100)
 (pi 1000)
--- char1/scheme/simpsonIntegral.rkt
+++ char1/scheme/simpsonIntegral.rkt
@@ -1,47 +1,33 @@
 #lang sicp
 
 (define (cube x)
-  (* x x x)
-)
+  (* x x x))
 
 (define (sum term a next b)
-  (if (> a b)
-      0
-      (+ (term a)
-         (sum term (next a) next b)))
-)
+  (if (> a b) 0 (+ (term a) (sum term (next a) next b))))
 
 (define (isEven n)
-  (= remainder(n 2) 0)
-)
+  (= remainder (n 2) 0))
 
 (define (simpson func a b n)
   (define h (/ (- b a) n))
 
   (define (y k)
-    (func (+ a (* k h)))
-  )
+    (func (+ a (* k h))))
 
   (define (factor k)
-    (cond ((or (= k 0) (= k n)) 1)
-          ((isEven k) 2)
-          (else 4)
-    )
-  )
+    (cond
+      [(or (= k 0) (= k n)) 1]
+      [(isEven k) 2]
+      [else 4]))
 
   (define (term k)
-    (* (factor k)
-       (y k))
-  )
+    (* (factor k) (y k)))
 
   (define (next k)
-    (+ k 1)
-  )
+    (+ k 1))
 
-   (* (/ h 3)
-      (sum term 0 next n))
-)
-
+  (* (/ h 3) (sum term 0 next n)))
 
 (simpson cube 0 1 100)
 ; (simpson cube 0 1 1000)
