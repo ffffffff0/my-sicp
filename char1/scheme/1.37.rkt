8d3206b0cfa08f254a8d6281e2104f06d90cb504
wenxu <xuwenmail@foxmail.com>
2023-07-30 14:53:55 +0800

    update

--- char1/scheme/1.37.rkt
+++ char1/scheme/1.37.rkt
@@ -1,6 +1,22 @@
 #lang sicp
 
+; recursive
 (define (cont-frac n d k)
-  ())
+  (define (rec-item idx)
+    (if (> idx k)
+        0
+        (/ n
+           (+ d
+              (rec-item (+ 1 idx))))))
+  (rec-item 1))
 
-(cont-frac (lambda (x)))
+; iterative
+; (define (cont-frac n d k)
+;   (define (iter-item idx result)
+;     (if (> idx k)
+;         result
+;         (iter-item (+ idx 1) ()))))
+
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           10)
