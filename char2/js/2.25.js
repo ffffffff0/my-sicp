3a75dda15ff5be32bcb0333233b37801ebdf97b4
wen_xu <wen_xu@trendmicro.com>
2023-09-07 18:04:08 +0800

    update 2.25.js

---
+++ char2/js/2.25.js
@@ -0,0 +1,27 @@
+const x = [[1, 2], 3, [4, 5], [8, 9, 10], [6, 7], 10];
+
+function car(aList) {
+    return Array.isArray(aList) ? aList[0] : null;
+}
+function cdr(aList) {
+  return aList.length === 0 ? aList[0] : aList.slice(1);
+}
+function isPair(aList) {
+  return Array.isArray(aList);
+}
+
+const reverse_iter = function(aList, ret) {
+    console.log('ret: ' + JSON.stringify(ret));
+    if (!aList) {
+        return ret;
+    }
+
+    return reverse_iter(cdr(aList), [car(aList)].concat(ret));
+};
+
+const reverse = function(aList) {
+    return reverse_iter(aList, []);
+};
+
+console.log('x: ' + JSON.stringify(x));
+console.log('reverse(x): ' + JSON.stringify(reverse(x)));
