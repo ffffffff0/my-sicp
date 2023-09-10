eb92b1f336e31bd58b00fc272858c06c360ddd37
wen xu <xuwenmail@foxmail.com>
2023-09-10 22:15:36 +0800

    update

--- char2/scheme/2.32.rkt
+++ char2/scheme/2.32.rkt
@@ -7,17 +7,40 @@
 ;       (append (list list1 (car list2))
 ;               (cdr list2))))
 
-(define (append list1 list2)
-  (if (null? list1)
-      list2
-      (cons (car list1) (append (cdr list1) list2))))
+; (define (append list1 list2)
+;   (if (null? list1)
+;       list2
+;       (cons (car list1) (append (cdr list1) list2))))
 
 (append (list 1 2 3) (list 4 5 6))
+
+; (define (subsets s)
+;   (if (null? s)
+;       (list nil)
+;       (let ((rest (subsets (cdr s))))
+;         (append rest (map (lambda (x) (cons (car s) x)) rest)))))
 
 (define (subsets s)
   (if (null? s)
       (list nil)
-      (let ((rest (subsets (cdr s))))
-        (append rest (map (lambda (x) (cons (car s) x)) rest)))))
+      (append (subsets (cdr s))
+              (map (lambda (x)
+                     (cons (car s) x))
+                   (subsets (cdr s))))))
 
 (subsets (list 1 2 3))
+
+#|
+  (subsets (1 2 3))
+  (append (subsets (2 3)) (map func (subsets (2 3)))) next step:
+(append ((3) (2 3)) (map func ((3) (2 3))))
+(append ((3) (2 3)) ((1 3) (1 2 3))
+((3) (2 3) (1 3) (1 2 3))
+
+(subsets (2 3))
+(append (subsets (3)) (map func (subsets (3)))) next step:
+= ((3) (2 3))
+
+(subsets (3))
+(append nil (map func nil)) = (append nil (3)) = (3)
+|#
